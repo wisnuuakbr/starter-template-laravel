eeaea2ed2cd65f56bbdd30afafe6221557d1aed3
wisnuaero <wisnuaero3@gmail.com>
2023-07-08 11:13:06 +0700

    add key to seeder

--- database/seeders/NavigationSeeder.php
+++ database/seeders/NavigationSeeder.php
@@ -16,6 +16,7 @@ class NavigationSeeder extends Seeder
     public function run()
     {
         Navigation::create([
+            'id'            => '01001',
             'name'          => 'Home',
             'url'           => 'home',
             'icon'          => 'typcn typcn-home-outline',
@@ -24,6 +25,7 @@ class NavigationSeeder extends Seeder
 
         ]);
         Navigation::create([
+            'id'            => '01002',
             'name'          => 'Settings',
             'url'           => 'settings',
             'icon'          => 'typcn typcn-cog-outline',
@@ -32,21 +34,26 @@ class NavigationSeeder extends Seeder
 
         ]);
         Navigation::create([
+            'id'            => '01003',
             'name'          => 'Navigations',
             'url'           => 'settings/navigations',
             'icon'          => '',
+            'parent_id'     => '01002',
             'description'   => 'Pengaturan Navigasi Menu Aplikasi',
             'display_st'    => '1'
 
         ]);
         Navigation::create([
+            'id'            => '01004',
             'name'          => 'Management Users',
             'url'           => 'settings/users',
             'icon'          => '',
+            'parent_id'     => '01002',
             'description'   => 'Pengaturan User',
             'display_st'    => '1'
         ]);
         Navigation::create([
+            'id'            => '01005',
             'name'          => 'Permissions',
             'url'           => 'permissions',
             'icon'          => 'typcn typcn-group-outline',
